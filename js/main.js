8e171b1fc3d20c35185858647f03c98ec6953f52
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2021-06-05 03:15:55 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,9 +79,11 @@ $('.navbar-collapse a').click(function(){
 
 // $(document).ready(function() {
 //     $('.carousel').carousel({
-//       interval: 1200
+//         interval: 120
 //     })
-//   });
+// });
+
+
 //=====  End of carousel block  ======
 
 
@@ -92,18 +94,18 @@ $('.navbar-collapse a').click(function(){
 //=============================================
 
 // Video Play
-$(function () {
-    // Auto play modal video
-    $(".video").click(function () {
-        var theModal = $(this).data("target"),
-            videoSRC = $(this).attr("data-video"),
-            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
-        $(theModal + ' iframe').attr('src', videoSRCauto);
-        $(theModal + ' button.close').click(function () {
-            $(theModal + ' iframe').attr('src', videoSRC);
-        });
-    });
-});
+// $(function () {
+//     // Auto play modal video
+//     $(".video").click(function () {
+//         var theModal = $(this).data("target"),
+//             videoSRC = $(this).attr("data-video"),
+//             videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+//         $(theModal + ' iframe').attr('src', videoSRCauto);
+//         $(theModal + ' button.close').click(function () {
+//             $(theModal + ' iframe').attr('src', videoSRC);
+//         });
+//     });
+// });
 
 //=====  End of Section comment block  ======
 
